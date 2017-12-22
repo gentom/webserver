d17957ef9d0b09fd9fe9a48c5002c48d8bb20f81
Gento Morikawa <gento.m.developer@gmail.com>
2017-12-22 16:23:01 +0900

    Update server.c

--- udp/server/server.c
+++ udp/server/server.c
@@ -1,36 +1,48 @@
-// UDP-Server
-// server port
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <ctype.h>
 
-#define BUFSIZE 512
+int main(int argc, char** argv){
+    int sockfd, n;
+    int cl; /* client length */
+    struct sockaddr_in ca; /* client address */
+    char buf[512]; /* 文字列受信用 */
+    char *c = buf;
 
-int main(int argc, char **argv){
-    int sockfd; //ソケット
-    int port; //ポート番号
-    int clientLength; //クライアントアドレスのバイトサイズ
-    struct sockaddr_in serverAddr; //サーバーアドレス
-    struct sockaddr_in clientAddr; //クライアントアドレス
-    struct hostent *hostp; //クライアントホスト情報
-    char msg[BUFSIZE]; //メッセージ・buffer
-    char *hostaddrp; //ドット付き十進数で表されたホストアドレス
-    int optValue; //setsockoptのフラグ値
-
-    /* 
-    * コマンドライン引数のチェック 
-    */
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s <port>\n", argv[0]);
-        exit(1);
+    if(argc!=2){
+        fprintf(stderr, "invalid arguments\n");
+        return EXIT_FAILURE;
     }
-    port = atoi(argv[1]);
 
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
+    ca.sin_family = AF_INET;
+    ca.sin_addr.s_addr = htonl(INADDR_ANY);
+    ca.sin_port = htons(atoi(argv[1]));
+
+    /* ソケットにバインド */
+    if(bind(sockfd, (struct sockaddr *)&ca, sizeof(ca)) < 0){
+        perror("failed to bind socket");
+        return EXIT_FAILURE;
+    }
+
+    for(;;){
+        cl = sizeof(ca); /* クライアントの長さ(サイズ)を取得 */
+        n = recvfrom(sockfd, buf, 512, 0, (struct sockaddr *)&ca, (socklen_t *)&cl);
+        buf[n] = '\0';
+        c = buf;
+        while(*c){
+            *c = (char)toupper((int)*c); /* 受信した文字を大文字に直す */
+            c++;
+        }
+        sendto(sockfd, buf, n, 0, (struct sockaddr *)&ca, cl); /* クライアントへ送信 */
+        fputs(buf, stdout); /* 受信した文字列を全て書き出し */
+        fflush(stdout);
+    }
+    return EXIT_SUCCESS;
 }
