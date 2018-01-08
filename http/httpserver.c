343a2255738bc46a9089b4e727a22c2b909250e6
gentom <gento.m.developer@gmail.com>
2018-01-08 13:48:49 +0900

    Simple HTTP Server

--- http/httpserver.c
+++ http/httpserver.c
@@ -1,64 +1,83 @@
-#include <stdio.h> //printf(), fprintf(), perror()
-#include <sys/socket.h> //socket(), bind(), accept(), listen()
-#include <arpa/inet.h> // struct sockaddr_in, struct sockaddr, inet_ntoa()
-#include <stdlib.h> //atoi(), exit(), EXIT_FAILURE, EXIT_SUCCESS
-#include <string.h> //memset()
-#include <unistd.h> //close()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h> //fork
+
+#define DEFAULT_SERV_TCP_PORT 8080
+
+int main(int argc, char** argv) {
+	
+	int sockfd, len, s, n, port;
+	struct sockaddr_in ca;
+	char buf[1024];
+	char scode[] = "HTTP1.0 200 OK\n";
+	char eoh[] = "\n";
+	FILE *fp;
+
+	switch(argc) {
+					case 1:
+									port = DEFAULT_SERV_TCP_PORT;
+									break;
+					case 2:
+									port = atoi(argv[1]);
+									break;
+					default:
+									fprintf(stderr, "invalid arguments.\n");
+									return EXIT_FAILURE;
+	}	
 
 
-#define DEFAULT_SERV_TCP_PORT 8888 // default port: 8888
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-int main(int argc, char** argv){
-    int sockfd, len, s, n, port;
-    struct sockaddr_in ca;
-    char buf[1024];
-    char scode[] = "HTTP/1.0 200 OK\n";
-    char eoh[] = "\n";
-    FILE *fp;
+	ca.sin_family = AF_INET;
+	ca.sin_addr.s_addr = htonl(INADDR_ANY);
+	ca.sin_port = htons(port);
 
-    switch(argc){
-    case 1:
-        port = DEFAULT_SERV_TCP_PORT;
-        break;
-    case 2:
-        port = atoi(argv[1]);
-        break;
-    default:
-        fprintf(stderr, "invalid arguments. \n");
-        return EXIT_FAILURE;
-    }
+	if (bind(sockfd, (struct sockaddr *)&ca, sizeof(ca)) == -1) {
+		perror("bind");
+		return EXIT_FAILURE;
+	}
+  
+  // 待ち合わせを行う
+	listen(sockfd, 5);
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	len = sizeof(ca);
 
-    ca.sin_family = AF_INET;
-    ca.sin_addr.s_addr = htonl(INADDR_ANY);
-    ca.sin_port = htons(port);
+  // 相手と接続する
+	s = accept(sockfd, (struct sockaddr *)&ca, (socklen_t *) &len);
 
-    // if fail on binding
-    if(bind(sockfd, (struct sockaddr *) &ca, sizeof(ca)) == -1 ){
-        perror("bind");
-        return EXIT_FAILURE;
-    }
+  // アドレス情報を表示する
+	printf("%s:%d \n", inet_ntoa(ca.sin_addr), ntohs(ca.sin_port));
 
-    listen(sockfd, 5);
+  // リクエストを受信する
+	n = recv(s, buf, sizeof(buf), 0);
 
-    len = sizeof(ca);
-    s = accept(sockfd, (struct sockaddr *) &ca, (socklen_t *) &len);
+	buf[n] = 0;
 
+	printf("%s, len=%d", buf, (int)strlen(buf));
 
-    n = recv(s, buf, sizeof(buf), 0);
-    buf[n] = 0;
+	send(s, scode, strlen(scode), 0);
+	send(s, eoh, strlen(eoh), 0);
 
+	fp = fopen("index.html", "r");
+	while ((n = fread(buf, 1, 1024, fp)) > 0) {
+		send(s, buf, n, 0);
+	}
 
-    send(s, scode, strlen(scode), 0);
-    send(s, eoh, strlen(eoh), 0);
-
-    fp = fopen("index.html", "r");
-    while ((n = fread(buf, 1, 1024, fp)) > 0){
-        send(s, buf, n, 0);
-    }
-    fclose(fp);
-    close(s);
-    return EXIT_SUCCESS;
+	fclose(fp);
+	close(s);
+	return EXIT_SUCCESS;
 
 }
+
+
+
+
+
+/* vim: set sw=2 ts=2 expandtab nowrap : */
+
